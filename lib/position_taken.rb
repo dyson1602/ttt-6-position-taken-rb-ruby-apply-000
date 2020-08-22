d215bb184617b61e3c614ca2394f6d78ee644696
Christopher Clark <cclark000@citymail.cuny.edu>
2020-08-22 19:45:40 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,13 +1,9 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[index] == " "
-    FALSE
-  elsif board[index] == ""
+  if board[index] == " " || board[index] == "" || board[index] == nil
     FALSE
   elsif board[index] == "X" || "O"
     TRUE
-  elsif board[index] == nil
-    FALSE
   end
 end
